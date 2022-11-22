28343ba6b949615d9263b179a4a0f9c0d6a8dd06
Brian dos Reis <brazdore@gmail.com>
2022-11-22 01:58:04 -0300

    design-patterns-java: Singleton pt. 2

--- java/design-patterns-java/src/main/java/org/example/patterns/creational/Singleton.java
+++ java/design-patterns-java/src/main/java/org/example/patterns/creational/Singleton.java
@@ -4,20 +4,25 @@ import java.io.*;
 
 public class Singleton {
     public static void main(String[] args) throws Exception {
-        BasicSingleton instance = BasicSingleton.getInstance();
-        instance.setValue(2);
-        System.out.println(instance);
+        // BasicSingleton instance = BasicSingleton.getInstance();
+        // instance.setValue(2);
+        // System.out.println(instance);
 
-        System.out.println("---");
+        // System.out.println("---");
 
-        String filename = "singleton.bin";
-        InputOutput.save(instance, filename);
-        BasicSingleton deserialized = InputOutput.read(filename); // Bad! Multiple instances of a Singleton can be created through Reflection or Serialization. Must implement custom readResolve() on BasicSingleton.
-        System.out.println(deserialized == instance);
+        // String filename = "singleton.bin";
+        // InputOutput.save(instance, filename);
+        // BasicSingleton deserialized = InputOutput.read(filename); // Bad! Multiple instances of a Singleton can be created through Reflection or Serialization. Must implement custom readResolve() on BasicSingleton.
+        // System.out.println(deserialized == instance);
 
-        System.out.println("---");
+        // System.out.println("---");
+
+        // StaticBlockSingleton instance = StaticBlockSingleton.getInstance();
+        // System.out.println(instance);
+
     }
 
+    // Basic Singleton
     static class BasicSingleton implements Serializable {
         private static final BasicSingleton INSTANCE = new BasicSingleton();
 
@@ -66,4 +71,70 @@ public class Singleton {
             }
         }
     }
+
+    // Static block Singleton
+    static class StaticBlockSingleton {
+        private static StaticBlockSingleton instance;
+
+        static {
+            try {
+                instance = new StaticBlockSingleton();
+            } catch (RuntimeException e) {
+                System.out.println("Error when initializing singleton: " + e);
+            }
+        }
+
+        private StaticBlockSingleton() throws RuntimeException {
+            System.out.println("Singleton is initializing.");
+            throw new RuntimeException();
+        }
+
+        public static StaticBlockSingleton getInstance() {
+            return instance;
+        }
+    }
+
+    // Lazy & thread-Safe singleton
+    static class LazySingleton {
+        private static LazySingleton instance;
+
+        private LazySingleton() {
+            System.out.println("Initializing LazySingleton.");
+        }
+
+        // Sync whole method. Not very good.
+        /*public static synchronized LazySingleton getInstance() {
+            if (instance == null) {
+                instance = new LazySingleton();
+            }
+            return instance;
+        }*/
+
+        // Double-checked locking. A little outdated, kind of OK still.
+        public static LazySingleton getInstance() {
+            if (instance == null) {
+                synchronized (LazySingleton.class) {
+                    if (instance == null) {
+                        instance = new LazySingleton();
+                    }
+                }
+            }
+            return instance;
+        }
+    }
+
+    // Static nested approach. Thread-safe and simpler.
+    static class InnerStaticSingleton {
+        private InnerStaticSingleton() {
+        }
+
+        public static InnerStaticSingleton getInstance() {
+            return Impl.INSTANCE;
+        }
+
+        private static class Impl {
+            private static final InnerStaticSingleton INSTANCE = new InnerStaticSingleton();
+        }
+    }
+
 }
