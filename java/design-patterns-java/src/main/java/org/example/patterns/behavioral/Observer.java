e2116e57887b7829c62fec63981ced90a827478b
Brian dos Reis <brazdore@gmail.com>
2023-01-04 02:07:38 -0300

    design-patterns-java: Observer pt. 2

--- java/design-patterns-java/src/main/java/org/example/patterns/behavioral/Observer.java
+++ java/design-patterns-java/src/main/java/org/example/patterns/behavioral/Observer.java
@@ -1,11 +1,29 @@
 package org.example.patterns.behavioral;
 
+import org.example.utils.Line;
+
+import java.io.Closeable;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
 
 public class Observer {
     public static void main(String[] args) {
         ObserverInfrastructureDemo observerInfrastructureDemo = new ObserverInfrastructureDemo();
+        Line.split();
+
+        HandmadeEventsDemo.main(new String[]{"a", "b", "c"});
+        Line.split();
+
+        Game game = new Game();
+        Rat r1 = new Rat(game);
+        Rat r2 = new Rat(game);
+        Rat r3 = new Rat(game);
+        r2.close();
+        System.out.println(r3.attack);
     }
 
     // Observer & Observable
@@ -72,4 +90,131 @@ public class Observer {
         }
     }
 
+    // Event Class
+    static class Event<T> {
+        private int count = 0;
+        private final Map<Integer, Consumer<T>> handlers = new HashMap<>();
+
+        public Subscription addHandler(Consumer<T> handler) {
+            int i = count;
+            handlers.put(count++, handler);
+            return new Subscription(this, i);
+        }
+
+        public void fire(T args) {
+            handlers.values().forEach(h -> h.accept(args));
+        }
+
+        public class Subscription implements AutoCloseable {
+            private final Event<T> event;
+            private final int id;
+
+            public Subscription(Event<T> event, int id) {
+                this.event = event;
+                this.id = id;
+            }
+
+            @Override
+            public void close() {
+                event.handlers.remove(id);
+            }
+        }
+    }
+
+    static class PropertyChangeEventArgs {
+        public Object source;
+        public String propertyName;
+
+        public PropertyChangeEventArgs(Object source, String propertyName) {
+            this.source = source;
+            this.propertyName = propertyName;
+        }
+    }
+
+    static class Human {
+        public Event<PropertyChangeEventArgs> propertyChange = new Event<>();
+
+        private int age;
+
+        public int getAge() {
+            return age;
+        }
+
+        public void setAge(int age) {
+            if (this.age == age) {
+                return;
+            }
+
+            boolean oldCanVote = getCanVote();
+
+            this.age = age;
+            propertyChange.fire(new PropertyChangeEventArgs(this, "age"));
+
+            if (oldCanVote != getCanVote()) {
+                propertyChange.fire(new PropertyChangeEventArgs(this, "canVote"));
+            }
+        }
+
+        public boolean getCanVote() { // Depends on field age, therefore change notifications must be inside setAge(); which is really bad for scaling.
+            return age >= 18;
+        }
+    }
+
+    static class HandmadeEventsDemo {
+        public static void main(String[] args) {
+            Human human = new Human();
+            Event<PropertyChangeEventArgs>.Subscription sub = human.propertyChange.addHandler(
+                    x -> System.out.println("Person's " + x.propertyName + " has changed"));
+            human.setAge(17);
+            human.setAge(18);
+            sub.close();
+            human.setAge(19);
+        }
+    }
+
+    // Exercise
+    static class EventExercise<T> {
+        private final List<BiConsumer<Object, T>> consumers = new ArrayList<>();
+
+        public void subscribe(BiConsumer<Object, T> consumer) {
+            consumers.add(consumer);
+        }
+
+        public void invoke(Object sender, T arg) {
+            consumers.forEach(c -> c.accept(sender, arg));
+        }
+    }
+
+    static class Game {
+        public EventExercise<Void> ratEnters = new EventExercise<>();
+        public EventExercise<Void> ratDies = new EventExercise<>();
+        public EventExercise<Rat> notifyRat = new EventExercise<>();
+    }
+
+    static class Rat implements Closeable {
+        private final Game game;
+        public int attack = 1;
+
+        public Rat(Game game) {
+            this.game = game;
+            game.ratEnters.subscribe((sender, arg) -> {
+                if (sender != this) {
+                    attack++;
+                    game.notifyRat.invoke(this, (Rat) sender);
+                }
+            });
+            game.notifyRat.subscribe((sender, rat) -> {
+                if (rat == this) {
+                    attack++;
+                }
+            });
+            game.ratDies.subscribe((sender, arg) -> --attack);
+            game.ratEnters.invoke(this, null);
+        }
+
+        @Override
+        public void close() {
+            game.ratDies.invoke(this, null);
+        }
+    }
 }
